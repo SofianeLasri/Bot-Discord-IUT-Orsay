ce7006343b73921864d0b40147ad07f9cd6202f3
SofianeLasri <alasri250@gmail.com>
2021-09-05 18:59:41 +0200

    toString?

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ async function checkAnniv() {
 	});
 	console.log('['+'SUCCES'.brightGreen+'] C\'est l\'anniversaire de '+count+' personne(s).');
 	for await (const member of rows){
-		console.log(member.memberId);
+		console.log(member.memberId.toString());
 	}
 }
 
