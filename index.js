e9a54217751947c9a228144062001e93b1949d11
SofianeLasri <alasri250@gmail.com>
2021-09-05 19:07:08 +0200

    AAAH

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ async function checkAnniv() {
 	});
 	console.log('['+'SUCCES'.brightGreen+'] C\'est l\'anniversaire de '+count+' personne(s).');
 	for await (const member of rows){
-		let user = client.users.cache.fetch(member.memberId.toString());
+		let user = client.users.fetch(member.memberId.toString());
 		console.log(' 🎂 '+user.username);
 	}
 }
