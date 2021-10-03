0ff604ad9f2ea2c27cbe93b72d524682669259a2
SofianeLasri <alasri250@gmail.com>
2021-10-03 23:44:04 +0200

    moment.unix

--- index.js
+++ index.js
@@ -414,7 +414,7 @@ client.on('messageCreate', async message => {
 						+ (currentdate.getHours()+1) + ":"	
 						+ currentdate.getMinutes() + ":" 
 						+ currentdate.getSeconds();*/
-	let datetime = moment(message.createdTimestamp, "yyy-MM-dd hh:mm:ss");
+	let datetime = moment.unix(message.createdTimestamp).format("yyy-MM-dd hh:mm:ss");
 	console.log(datetime);
 	await discordMessages.create({
 		messageId: message.id,
